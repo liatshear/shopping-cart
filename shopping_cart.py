ab784ec557ea0680f2b4960d8ab2a79fef94aaeb
Liat Shear <liat.shear@post.idc.ac.il>
2021-11-29 11:01:21 +0200

    remove and get subtotal

--- shopping_cart.py
+++ shopping_cart.py
@@ -3,16 +3,20 @@ from item import Item
 
 class ShoppingCart:
     def add_item(self, item: Item):
-        if not isinstance(Item, self): 
+        if not isinstance(Item, self):
             self.items.append(Item)
         else:
-            raise ItemAlreadyExistsError("Item Already Exists Error")          
+            raise ItemAlreadyExistsError("Item Already Exists Error")
 
 
     def remove_item(self, item_name: str):
-        # TODO: Complete
-        pass
+        if isinstance(str, self):
+            self.items.remove(str)
+        else:
+            raise ItemNotExistError("ItemA NOt Exist Error")
 
     def get_subtotal(self) -> int:
-        # TODO: Complete
-        pass
+        total =0
+        for self in ShoppingCart:
+            total += self.price
+        print(total)
