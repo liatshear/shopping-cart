03c6aef0d9e7839f2763389cdb556b2defee24e9
Liat Shear <liat.shear@post.idc.ac.il>
2021-11-29 10:58:32 +0200

    first edit

--- shopping_cart.py
+++ shopping_cart.py
@@ -3,8 +3,11 @@ from item import Item
 
 class ShoppingCart:
     def add_item(self, item: Item):
-        # TODO: Complete
-        pass
+        if not isinstance(Item, self): 
+            self.items.append(Item)
+        else:
+            raise ItemAlreadyExistsError("Item Already Exists Error")          
+
 
     def remove_item(self, item_name: str):
         # TODO: Complete
