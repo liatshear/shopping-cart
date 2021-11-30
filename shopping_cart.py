656fc939ff59d5c12b27f88d9da3608c8f145cdb
Liat Shear <liat.shear@post.idc.ac.il>
2021-11-30 17:04:42 +0200

    items edit

--- shopping_cart.py
+++ shopping_cart.py
@@ -4,14 +4,14 @@ from errors import ItemNotExistError, ItemAlreadyExistsError
 
 class ShoppingCart:
     def add_item(self, item: Item):
-        if not isinstance(item, Item):
-            self.items.append(Item)
+        if item not in self.items:
+            self.items.append(item)
         else:
             raise ItemAlreadyExistsError("Item Already Exists Error")
 
 
     def remove_item(self, item_name: str):
-        if isinstance(str, item):
+        if str in self.items.name:
             self.items.remove(str)
         else:
             raise ItemNotExistError("ItemA NOt Exist Error")
