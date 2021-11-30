a4a38dc85f59c9bc2aedf2ed65c7d47a9183b538
Liat Shear <liat.shear@post.idc.ac.il>
2021-11-30 18:23:19 +0200

    first test shopping cart

--- shopping_cart.py
+++ shopping_cart.py
@@ -6,7 +6,7 @@ from errors import ItemNotExistError, ItemAlreadyExistsError
 class ShoppingCart:
     def add_item(self, item: Item):
         if item not in self.items:
-            self.items.append(item)
+            self._items.append(item)
         else:
             raise ItemAlreadyExistsError("Item Already Exists")
 
@@ -14,13 +14,13 @@ class ShoppingCart:
     def remove_item(self, item_name: str):
         for item in self.items:
             if item.name == item_name:
-                self.items.remove(item)
+                self._items.remove(item)
                 break
         else:
             raise ItemNotExistError("Item does not Exist Error")
 
     def get_subtotal(self) -> int:
         total = 0
-        for item in self.items:
+        for item in self._items:
             total += item.price
         return total
