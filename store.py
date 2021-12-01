0561ac094385d5c0bc67ed6df43520250f7cc924
Liat Shear <liat.shear@post.idc.ac.il>
2021-12-01 17:55:23 +0200

    big fix

--- store.py
+++ store.py
@@ -23,6 +23,41 @@ class Store:
     def get_items(self) -> list:
         return self._items
 
+    def check_hashtag(self, hashtag:str):
+        check = 0 ## variable to check if item in cart or not a match
+        itemList = self.get_items()
+        for item in ShoppingCart:
+            for item.hashtag in item.hashtags:
+                if(item.hashtag == hashtag):
+                    return False
+        for item in itemList: # iterate through store to check hashtags but not substring
+            for item.hashtag in item.hashtags:
+                if((item.hashtag == hashtag) and (len(hashtag) == len(item.hashtag))):
+                    check = 1
+        if check == 1:
+            return True
+        else:
+            return False
+
+    def check_name(self, item_name:str):
+        check = 0 ## variable to check if item is in cart or not a match
+        itemList = self.get_items()
+        for item in ShoppingCart:
+            if(item_name == item.name):
+                return False
+        for item in itemList: ## iterate through the items in the store to see if the search matches
+            if((item_name in item.name) or (item_name == item.name)): ## if the search matches/is substring of item
+                check = 1
+       
+            
+        if check == 1:
+            return True
+        else:
+            return False
+
+
+
+
     def search_by_name(self, item_name: str) -> list:
         ReturnList = []
         itemList = self.get_items()
@@ -48,30 +83,29 @@ class Store:
         itemList = self.get_items()
         for item in self._items:   
             if(item.name == item_name):
-                raise ItemAlreadyExistsError("This item already e[xists")
+                raise ItemAlreadyExistsError("This item already exists")
         for item in itemList:
                 if((item_name == item.name) or (item_name in item.name)):
                     if count == 1:
                         raise TooManyMatchesError("Too many items matched")
                     else:
-                        self._shopping_cart.append(item)
+                        ShoppingCart().add_item(self, item)
                         count+=1
         if count == 0:
-            raise ItemNotExistError("ItemA NOt Exist Error")
+            raise ItemNotExistError("Item does not Exist Error")
         
             
     def remove_item(self, item_name: str):
-        itemList = self.get_items()
         count = 0
-        for item in itemList:
+        for item in self._items:
             if((item.name == item_name) or (item_name in item.name)):
                 count +=1
                 if count == 1:
-                    self._shopping_cart.remove(item)
+                    ShoppingCart().remove_item(self, item.name)
                 elif count > 1:
                     raise TooManyMatchesError("Too many matches found")
         if count == 0:
             raise ItemNotExistError("Item does not exist")
 
     def checkout(self) -> int:
-         return self.get_subtotal()
+         return ShoppingCart().get_subtotal(self)
