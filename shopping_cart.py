a60a3c5541c34501e83854534f0e42804ce2d178
Liat Shear <liat.shear@post.idc.ac.il>
2021-12-01 18:41:54 +0200

    comments ADDED

--- shopping_cart.py
+++ shopping_cart.py
@@ -4,24 +4,25 @@ from errors import ItemNotExistError, ItemAlreadyExistsError
 
 
 class ShoppingCart:
-    items = []
+    items = [] ## initialize items in shopping cart
     def add_item(self, item: Item):
-        if item in self.items:
+        if item not in self.items: ## check if item isnt already in cart
+            self.items.append(item) ## add item to cart
+        else: ## item is already in cart
             raise ItemAlreadyExistsError("Item Already Exists")
-        else:
-            self.items.append(item)
+            
 
            
     def remove_item(self, item_name: str):
         for item in self.items:
-            if item.name == item_name:
-                self.items.remove(item)
+            if item.name == item_name: ## check if the item name is in cart
+                self.items.pop(item) ## remove from cart
                 break
-        else:
+        else: ## item not in cart
             raise ItemNotExistError("Item doesn't Exist Error")
 
     def get_subtotal(self) -> int:
         total = 0
-        for item in self.items:
-            total += item.price
+        for item in self.items: ## iterate through items
+            total += item.price ## each time add the price of the item
         return total
