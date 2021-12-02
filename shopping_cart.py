2c8d93dbefabd0de1dab565d24fec439e33acc06
Liat Shear <liat.shear@post.idc.ac.il>
2021-12-03 01:48:40 +0200

    HUGE CHANGE

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,10 +1,11 @@
-import itertools
 from item import Item
 from errors import ItemNotExistError, ItemAlreadyExistsError
 
 
 class ShoppingCart:
-    items = []
+
+    def __init__(self):
+        self.items = []
 
     def add_item(self, item: Item):
         if item not in self.items: ## check if item isnt already in cart
@@ -15,14 +16,15 @@ class ShoppingCart:
            
     def remove_item(self, item_name: str):
         for item in self.items:
-            if item.name == item_name or item_name in item.name:
+            if (str(item.name) == str(item_name)):
                 self.items.remove(item) ## remove from cart  
-                break            
-                     ## item not in cart
+                return               
+        ## item not in cart
         raise ItemNotExistError("Item does not exist")
 
     def get_subtotal(self) -> int:
-        if not self.items:
+        if self.items:
+            return sum(item.price for item in self.items)
+        else:
             return 0
-        return sum(item.price for item in self.items) - 10
 
--- store.py
+++ store.py
@@ -2,6 +2,7 @@ from os import name
 import yaml
 import collections
 
+
 from item import Item
 from shopping_cart import ShoppingCart
 from errors import ItemNotExistError, ItemAlreadyExistsError, TooManyMatchesError
@@ -9,12 +10,12 @@ from errors import ItemNotExistError, ItemAlreadyExistsError, TooManyMatchesErro
 
 class Store:
 
-    items = []
     def __init__(self, path):
         with open(path) as inventory:
             items_raw = yaml.load(inventory, Loader=yaml.FullLoader)['items']
         self._items = self._convert_to_item_objects(items_raw)
         self._shopping_cart = ShoppingCart()
+        self.items = []
 
     @staticmethod
     def _convert_to_item_objects(items_raw):
@@ -31,27 +32,29 @@ class Store:
 
     def check_hashtag(self, hashtag:str):
         itemList = self.get_items()
+        ## check if item is in cart
         for item in self._shopping_cart.items:
             for item.hashtag in self._shopping_cart.items.hashtags:
-                if(item.hashtag == hashtag):
+                if(str(item.hashtag) == str(hashtag)):
                     return False ## item in cart therefore return False
-        for item in self._items: ## iterate through store to check hashtags but not substring
+        ## iterate through store to check hashtags but not substring
+        for item in itemList: 
             for item.hashtag in item.hashtags:
-                if((item.hashtag == hashtag) and (len(hashtag) == len(item.hashtag))):
+                if(str(item.hashtag) == str(hashtag)):
                     return True ## item is in the store AND not in the cart
         
         return False ## item is not in cart but also not in store
 
     ## method to check via name 1) item not in cart 2) item exists in store 
 
-    def check_name(self, item_name:str):
-        check = 0 ## variable to check if item is in cart or not a match
-        StoreitemList = self.get_items() ## put store items into list
+    def check_name(self, item):
+        item_name = item.name
+        itemList = self.get_items() ## put store items into list
         for item in self._shopping_cart.items: ## iterate through shopping cart to check if item exists
-            if(item_name == item.name):
+            if(str(item_name) == str(item.name)):
                 return False ## item already in cart
-        for item in self._items: ## iterate through the items in the store to see if the search matches
-            if((item_name in item.name) or (item_name == item.name)): ## if the search matches/is substring of item
+        for item in itemList: ## iterate through the items in the store to see if the search matches
+            if((str(item_name) in str(item.name)) or (str(item_name) == str(item.name))): ## if the search matches/is substring of item
                 return True ## item not in cart AND in store
         return False ## item not in cart AND not in store 
 
@@ -59,12 +62,14 @@ class Store:
 
 
     def search_by_name(self, item_name: str) -> list:
-        returnList = list(filter(self.check_name(item_name), self)) ## filter method to get all items relevant into list
-        return returnList.sort(key=lambda x: x.get(self.item.name))
+        returnList = list(filter(self.check_name, self.get_items())) ## filter method to get all items relevant into list
+        ##expected_items_list = list(filter(lambda item: item_name in item.name, self.get_items()))
+        return returnList.sort(key=lambda item: item.name)
+
 
 
     def search_by_hashtag(self, hashtag: str) -> list: ## same as before but now check using hashtag
-        returnList = list(filter(self.check_hashtag(hashtag), self._items))
+        returnList = list(filter(self.check_hashtag, self.get_items()))
         return returnList.sort(key=lambda x: x.get(name))
        
         
@@ -74,9 +79,9 @@ class Store:
         itemList = self.get_items()
         for item in self._shopping_cart.items:   ## begin to iterate through items
             if(item.name == item_name): 
-                raise ItemAlreadyExistsError("This item already exists") ## item already in the car
-        for item in self._items: ## look at items in the store
-                if((item_name == item.name) or (item_name in item.name)): ## find a match in store
+                raise ItemAlreadyExistsError("This item already exists") ## item already in the cart
+        for item in itemList: ## look at items in the store
+                if((str(item_name) == str(item.name)) or (str(item_name) in str(item.name))): ## find a match in store
                     count +=1 ## each time theres a match
                     itemMatch = item ## save which item it is that matched for the case that there is only one match
         if count > 1: ## more then one match 
@@ -84,18 +89,18 @@ class Store:
         if count == 0: ## no matches
             raise ItemNotExistError("Item does not exist")
         if count == 1: ## exactly one match
-            ShoppingCart.add_item(self, itemMatch)
+            self._shopping_cart.add_item(itemMatch)
              
        
             
     def remove_item(self, item_name: str):
         count = 0 ## variable to check multiple matches
         for item in self._shopping_cart.items:
-            if((item.name == item_name) or (item_name in item.name)):
+            if item_name == item.name:
                 count +=1
                 itemMatch = item
         if count == 1: ## only one item matched in cart
-            ShoppingCart.remove_item(self, itemMatch.name)
+            self._shopping_cart.remove_item(itemMatch.name)
         if count == 0: ## no items matched 
             raise ItemNotExistError("Item does not exist")
         if count > 1: ## more then one item matched in cart
@@ -103,6 +108,7 @@ class Store:
         
 
     def checkout(self) -> int:
-        if len(self._shopping_cart.items) == 0:
+        if not self._shopping_cart.items:
             return 0
-        return ShoppingCart.self._shopping_cart.get_subtotal()
+        else:
+            return self._shopping_cart.get_subtotal()
